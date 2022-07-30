d1a54ef9a08bd7479f137359fea9bfaf08e0e1e6
Ahmed Khalifa <ahmed70000@gmail.com>
2022-07-30 12:29:02 +0200

    Dynamic routes

--- pages/[city].tsx
+++ pages/[city].tsx
@@ -1,18 +1,19 @@
 import { Box, Flex } from "@chakra-ui/react";
 import axios from "axios";
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import NavBar from "../components/NavBar";
 import SunContainer from "../components/SunContainer";
 import WeatherForecast from "../components/WeatherForecast";
 import Script from "next/script";
 import { useEffect, useState } from "react";
+import Router, { useRouter } from "next/router";
 
-export async function getServerSideProps() {
+export const getServerSideProps: GetServerSideProps = async ({params}) =>  {
   let x: {};
   await axios
     .get(
-      "https://api.openweathermap.org/data/2.5/weather?q=Cairo&appid=009b0829f9d39cebc103cc0e0ca5be55&units=metric"
+      `https://api.openweathermap.org/data/2.5/weather?q=${params.city}&appid=009b0829f9d39cebc103cc0e0ca5be55&units=metric`
     )
     .then(async (res) => {
       await axios
@@ -62,43 +63,19 @@ const Home: NextPage = ({
   uv,
   weather,
 }: Props) => {
+  const router = useRouter()
+
   const [googleApiLoaded, setGoogleApiLoaded] = useState(false);
-  const [city, setCity] = useState("");
+  const [city, setCity] = useState(null);
   useEffect(() => {
-    let x = {};
-    axios
-      .get(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=009b0829f9d39cebc103cc0e0ca5be55&units=metric`
-      )
-      .then(async (res) => {
-        await axios
-          .get(
-            `https://api.openuv.io/api/v1/uv?lat=${res.data.coord.lat.toFixed(
-              2
-            )}&lng=${res.data.coord.lon.toFixed(2)}`,
-            {
-              headers: {
-                "x-access-token": "cb510185a58fe889ca8ee09a07c585bf",
-              },
-            }
-          )
-          .then((res) => (x = { uv: res.data.result.uv }));
-        x = {
-          ...x,
-          name: res.data.name,
-          weather: res.data.weather,
-          temp: res.data.main.temp,
-          humidity: res.data.main.humidity,
-          sunset: res.data.sys.sunset,
-          sunrise: res.data.sys.sunrise,
-        };
-      });
+    if(city)
+   router.push(`/${city}`)
   }, [city]);
 
   return (
-    <Box bgColor="primary.100">
+    <Box bgColor="primary.100" height="100vh">
       <Head>
-        <title>AccuWeather</title>
+        <title>AccuWeather - {city}</title>
       </Head>
       <Script
         src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVRVQQTQTN0mJtCCc107i_z1XfkEFE658&libraries=places"
