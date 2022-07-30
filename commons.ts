8a1775b9bae5409bbef643254b6d35f4bb71ab8e
Ahmed Khalifa <ahmed70000@gmail.com>
2022-07-30 14:19:31 +0200

    Edited  convertTime to add extra 0

--- commons.ts
+++ commons.ts
@@ -1,4 +1,10 @@
 export const convertTime = (timeStamp: number) => {
   const date = new Date(timeStamp * 1000);
-  return { hours: date.getHours, mins: date.getMinutes };
+  const hours = date.getUTCHours().toString();
+  const mins = date.getUTCMinutes().toString();
+
+  return {
+    hours: hours.length === 1 ? "0" + hours : hours,
+    mins: mins.length === 1 ? "0" + mins : mins,
+  };
 };
