533c6bc8383fc272582dc6d832661e5a4e6d43b3
Ahmed Khalifa <ahmed70000@gmail.com>
2022-08-02 22:38:34 +0200

    Added Weather type

--- types.ts
+++ types.ts
@@ -2,3 +2,21 @@ export interface Suggestion {
   id: string;
   displayName: string;
 }
+
+export interface Weather {
+  //City Name
+  name?: string;
+  uv?: number;
+  weather: string;
+  temp: number;
+  humidity?: number;
+  sunset?: number;
+  sunrise?: number;
+  time: number;
+  hourlyWeather?: [
+    {
+      temp: number;
+      time: string;
+    }
+  ];
+}
