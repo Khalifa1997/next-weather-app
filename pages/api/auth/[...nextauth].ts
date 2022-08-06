152d8f480ea9cd001fa2b4dedcfeec8316fb051f
Ahmed Khalifa <ahmed70000@gmail.com>
2022-08-06 23:10:13 +0200

    Added google provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
@@ -7,6 +9,9 @@ export default NextAuth({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
   ],
 });
