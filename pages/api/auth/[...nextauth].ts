7e5027dd7853e82206e69eb32ad6df69bdfa9cdc
Ahmed Khalifa <ahmed70000@gmail.com>
2022-08-12 19:14:08 +0200

    added secret to nextAuth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,7 @@ import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.SECRET,
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
