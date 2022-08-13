a92d49b3da6e9cbc9650aab45f3dd18f57d95524
Ahmed <ahmed70000@gmail.com>
2022-08-13 10:00:36 +0200

    added console.log to index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,6 +19,7 @@ import type { GetServerSideProps } from "next";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const session = await getSession(context);
+  console.log(process.env.NEXTAUTH_URL);
 
   if (session)
     return {
